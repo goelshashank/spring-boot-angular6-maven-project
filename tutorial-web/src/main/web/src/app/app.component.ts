2fc738a57aa1463b5c5c133e3eebd8116c6b4b10
b0218850 <F6$a.n0s>
2021-05-09 00:23:00 +0530

    test

--- tutorial-web/src/main/web/src/app/app.component.ts
+++ tutorial-web/src/main/web/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, Injectable} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Car } from './model/car';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+@Injectable()
 export class AppComponent {
-  title = 'np-app';
+
+     title = 'np-app';
+     carList: Car[] = [];
+     testing = 'hi';
+    constructor(private http: HttpClient) { }
+
+    getCars() {
+       this.http.get<Car[]>('http://localhost:8080/cool-cars').subscribe(
+           (response) => {
+             this.testing = 'bye';
+             this.carList = response;
+             alert(JSON.stringify(this.carList));
+             },
+           (error) => { console.log('Error happened' + JSON.stringify(error)); },
+           () => { alert('the subscription is completed'); });
+    }
 }
