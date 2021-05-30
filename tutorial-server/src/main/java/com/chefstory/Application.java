0adeec10117957eef2bb19e4f9e570d37499532d
b0218850 <F6$a.n0s>
2021-05-30 10:34:29 +0530

    fixed pojo

--- tutorial-server/src/main/java/com/chefstory/Application.java
+++ tutorial-server/src/main/java/com/chefstory/Application.java
@@ -12,7 +12,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
- * @author Swathi
+ * @author Shashank Goel
  *
  */
 @SpringBootApplication
