c53c2f11faf3681b289f9e95a4be9b8d0ab30175
shashank goel <goelshashank5556@gmail.com>
2023-01-01 13:39:46 +0530

    routing enabled

--- tutorial-web/src/main/chefstory/src/app/auth/auth-routing.module.ts
+++ tutorial-web/src/main/chefstory/src/app/auth/auth-routing.module.ts
@@ -8,16 +8,22 @@ import { ProfileComponent } from './profile/profile.component';
 import { BoardUserComponent } from './board-user/board-user.component';
 import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
 import { BoardAdminComponent } from './board-admin/board-admin.component';
+import {AuthComponent} from "./auth.component";
 
 const routes: Routes = [
-  { path: 'home.auth', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: 'user', component: BoardUserComponent },
-  { path: 'mod', component: BoardModeratorComponent },
-  { path: 'admin', component: BoardAdminComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full' }
+  {
+    path: '',
+    component: AuthComponent,
+  children:[
+      {path: 'login', component: LoginComponent},
+      {path: 'register', component: RegisterComponent},
+      {path: 'profile', component: ProfileComponent},
+      {path: 'user', component: BoardUserComponent},
+      {path: 'mod', component: BoardModeratorComponent},
+      {path: 'admin', component: BoardAdminComponent},
+      {path: 'home_auth', component: HomeComponent}
+  ]
+}
 ];
 
 
