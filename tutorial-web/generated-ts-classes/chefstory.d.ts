9c1dc4179ec6d2bd846a3476c4ecfa582b61b01e
shashank goel <goelshashank5556@gmail.com>
2024-05-11 14:58:01 +0530

    properties sql

--- tutorial-web/generated-ts-classes/chefstory.d.ts
+++ tutorial-web/generated-ts-classes/chefstory.d.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 2.35.1025 on 2024-05-11 14:48:10.
+// Generated using typescript-generator version 2.35.1025 on 2024-05-11 14:57:53.
 
 export interface BaseEntity extends Serializable, Cloneable {
     modifiedTs: Date;
